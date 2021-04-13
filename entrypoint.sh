8e35a64476c7489dcdd639c5c4f1161597840451
Will Licamele <wlicamele@bandwidth.com>
2021-04-13 16:44:49 -0400

    updates

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh -l
+bumpVersionType=$1
+bumpVersion=$2
+setVersion=$3
+pomLocations=$4
+bumpChangelog=$5
+changelogDesc=$6
 
-echo "Hello $1"
-time=$(date)
-echo ::set-output name=time::$time
+echo "Bump version type: $bumpVersionType"
+echo "Bump version: $bumpVersion"
+echo "Set version: $setVersion"
+echo "POM Location(s): $pomLocations"
+echo "Bump changelog $bumpChangelog"
+echo "Changelog Desc: $changelongDesc"
