859930ca7f32e44f2f120b7994413f5aa63250b9
WillL <33432945+wlicamele-bw@users.noreply.github.com>
2021-06-22 14:07:12 -0400

    remove checkout

--- entrypoint.sh
+++ entrypoint.sh
@@ -101,7 +101,6 @@ fi
 
 # Commit changes
 #git fetch
-git checkout ${GITHUB_HEAD_REF}
 git config user.name github-actions
 git config user.email github-actions@github.com
 for (( i=0; i<${#pomLocationsArray[@]}; i++ )); do
