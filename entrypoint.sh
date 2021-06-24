56b6bde945610e596d1bd211849f8c6ff557ac1a
WillL <33432945+wlicamele-bw@users.noreply.github.com>
2021-06-24 09:13:40 -0400

    git checkout instead of pull

--- entrypoint.sh
+++ entrypoint.sh
@@ -100,7 +100,7 @@ if [[ "$bumpChangelog" == "true" ]]; then
 fi
 
 # Commit changes
-git pull
+git checkout "${GITHUB_HEAD_REF}"
 git config user.name github-actions
 git config user.email github-actions@github.com
 for (( i=0; i<${#pomLocationsArray[@]}; i++ )); do
