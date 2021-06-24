3aa4e0fb50638f57c8ba4c91b52ecab31aa62ece
WillL <33432945+wlicamele-bw@users.noreply.github.com>
2021-06-24 16:14:54 -0400

    Removing echos

--- entrypoint.sh
+++ entrypoint.sh
@@ -102,18 +102,14 @@ if [[ "$bumpChangelog" == "true" ]]; then
 fi
 
 # Commit changes
-echo "git status: `git status`"
-echo "git checkout: `git checkout "${GITHUB_HEAD_REF}"`"
-
+git checkout "${GITHUB_HEAD_REF}"
 git config user.name github-actions
 git config user.email github-actions@github.com
-echo "set git username and email"
 for (( i=0; i<${#pomLocationsArray[@]}; i++ )); do
 	if test -f "${pomLocationsArray[$i]}"; then
         git add "${pomLocationsArray[$i]}"
     fi    
 done
 git add CHANGELOG.md
-
 git commit -m "GitHub Action - pom.xml and CHANGELOG.md Automations"
 git push origin HEAD:"${GITHUB_HEAD_REF}"
